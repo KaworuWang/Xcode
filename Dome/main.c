100d97335dab89880035539d49ce8a6cc0c17df2
Kaworu <kaworu@Kaworus-MacBook-Air.local>
2017-06-18 16:02:40 +0800

    Initial Commit

---
+++ Dome/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Dome
+//
+//  Created by Kaworu on 18/6/2017.
+//  Copyright © 2017 Kaworu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
